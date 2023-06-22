a4a5d89298d7785eb1f61a4362becc472fb5a062
Paula <paulinarybicka0114@gmail.com>
2023-06-22 22:13:31 +0200

    first_task

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# framework_test
+# Challenge_Portfolio
+
+# TASK 1 - Software configuration
+
+### Subtask 1 - Why did I choose to participate in the challenge?
+Hi! My name is Paulina and since the beginning of 2023 I am learning to become a QA. I started from manual testing first to get the most important basics. Through the process I got interested in quality assurance sector. After several months of learning both I also noticed that it would be helpful if I could understand programming better. It is very important when you want to provide products of the best quality. This naturally led me to automation testing which connects both.
--- test_cases/framework.py
+++ test_cases/framework.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.chrome.service import Service
 import os
 import unittest
 from selenium import webdriver
@@ -9,7 +10,8 @@ class Test(unittest.TestCase):
     @classmethod
     def setUp(self):
         os.chmod(DRIVER_PATH, 755)
-        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver_service = Service(executable_path=DRIVER_PATH)
+        self.driver = webdriver.Chrome(service=self.driver_service)
         self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
         self.driver.fullscreen_window()
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
@@ -23,3 +25,4 @@ class Test(unittest.TestCase):
 
     # Element of the first task: Try to search the Internet yourself how to get rid of the error:
     # "DeprecationWarning: executable_path has been deprecated, please pass in a Service object"
+
